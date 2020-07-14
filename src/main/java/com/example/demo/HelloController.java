0511c76ecd2c8eed2db81b81dcb1dffb5c6544b6
sandeephublikar <sandeep.hublikar@gmail.com>
2020-07-14 20:02:35 +0530

    third commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,7 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
